1118c9cafa1e8db7c05f8d6ec1bde42dafcaac56
Yoghourt <449088680@qq.com>
2016-06-29 17:12:39 +0800

    更换信鸽SDK

--- gen/com/mdground/screen/R.java
+++ gen/com/mdground/screen/R.java
@@ -327,53 +327,45 @@ containing a value of this type.
         public static final int verticalSpacing=0x7f010012;
     }
     public static final class color {
-        public static final int background_color=0x7f080000;
-        public static final int font=0x7f080002;
-        public static final int font_blue=0x7f080003;
-        public static final int font_dark=0x7f080001;
-        public static final int main_background=0x7f080005;
-        public static final int normal_black=0x7f080004;
-        public static final int normal_color=0x7f080006;
+        public static final int background_color=0x7f050000;
+        public static final int font=0x7f050002;
+        public static final int font_blue=0x7f050003;
+        public static final int font_dark=0x7f050001;
+        public static final int main_background=0x7f050005;
+        public static final int normal_black=0x7f050004;
+        public static final int normal_color=0x7f050006;
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
-         */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
-        public static final int layout_160=0x7f050016;
-        public static final int layout_30=0x7f050012;
-        public static final int layout_40=0x7f050013;
-        public static final int layout_60=0x7f050015;
-        public static final int layout_90=0x7f050014;
-        public static final int margin_10=0x7f05000c;
-        public static final int margin_120=0x7f05001f;
-        public static final int margin_14=0x7f05000d;
-        public static final int margin_15=0x7f05001c;
-        public static final int margin_18=0x7f050009;
-        public static final int margin_20=0x7f050010;
-        public static final int margin_240=0x7f05001e;
-        public static final int margin_26=0x7f05000e;
-        public static final int margin_3=0x7f05000a;
-        public static final int margin_33=0x7f050017;
-        public static final int margin_36=0x7f050018;
-        public static final int margin_38=0x7f050019;
-        public static final int margin_44=0x7f05001a;
-        public static final int margin_53=0x7f05001b;
-        public static final int margin_60=0x7f05001d;
-        public static final int margin_7=0x7f05000b;
-        public static final int margin_8=0x7f05000f;
-        public static final int margin_80=0x7f050011;
-        public static final int size_12=0x7f050002;
-        public static final int size_17=0x7f050003;
-        public static final int size_20=0x7f050004;
-        public static final int size_23=0x7f050005;
-        public static final int size_24=0x7f050006;
-        public static final int size_51=0x7f050007;
-        public static final int size_93=0x7f050008;
+        public static final int layout_160=0x7f060014;
+        public static final int layout_30=0x7f060010;
+        public static final int layout_40=0x7f060011;
+        public static final int layout_60=0x7f060013;
+        public static final int layout_90=0x7f060012;
+        public static final int margin_10=0x7f06000a;
+        public static final int margin_120=0x7f06001d;
+        public static final int margin_14=0x7f06000b;
+        public static final int margin_15=0x7f06001a;
+        public static final int margin_18=0x7f060007;
+        public static final int margin_20=0x7f06000e;
+        public static final int margin_240=0x7f06001c;
+        public static final int margin_26=0x7f06000c;
+        public static final int margin_3=0x7f060008;
+        public static final int margin_33=0x7f060015;
+        public static final int margin_36=0x7f060016;
+        public static final int margin_38=0x7f060017;
+        public static final int margin_44=0x7f060018;
+        public static final int margin_53=0x7f060019;
+        public static final int margin_60=0x7f06001b;
+        public static final int margin_7=0x7f060009;
+        public static final int margin_8=0x7f06000d;
+        public static final int margin_80=0x7f06000f;
+        public static final int size_12=0x7f060000;
+        public static final int size_17=0x7f060001;
+        public static final int size_20=0x7f060002;
+        public static final int size_23=0x7f060003;
+        public static final int size_24=0x7f060004;
+        public static final int size_51=0x7f060005;
+        public static final int size_93=0x7f060006;
     }
     public static final class drawable {
         public static final int app_icon=0x7f020000;
@@ -427,59 +419,58 @@ containing a value of this type.
         public static final int tab_selector=0x7f020030;
     }
     public static final class id {
-        public static final int LoginID=0x7f0a0026;
-        public static final int action_settings=0x7f0a0034;
-        public static final int alwaysScroll=0x7f0a000c;
-        public static final int auto_fit=0x7f0a0015;
-        public static final int bottom=0x7f0a0000;
-        public static final int btn_login=0x7f0a001e;
-        public static final int center=0x7f0a0001;
-        public static final int center_horizontal=0x7f0a0002;
-        public static final int center_vertical=0x7f0a0003;
-        public static final int clip_horizontal=0x7f0a0004;
-        public static final int clip_vertical=0x7f0a0005;
-        public static final int columnWidth=0x7f0a0011;
-        public static final int disabled=0x7f0a000d;
-        public static final int et_account=0x7f0a0019;
-        public static final int et_password=0x7f0a001c;
-        public static final int fill=0x7f0a0006;
-        public static final int fill_horizontal=0x7f0a0007;
-        public static final int fill_vertical=0x7f0a0008;
-        public static final int gridview=0x7f0a002d;
-        public static final int gvp=0x7f0a001f;
-        public static final int horizontal=0x7f0a000f;
-        public static final int iv_account=0x7f0a0018;
-        public static final int iv_avatar=0x7f0a0029;
-        public static final int iv_clear_account=0x7f0a001a;
-        public static final int iv_clear_password=0x7f0a001d;
-        public static final int iv_clip=0x7f0a002e;
-        public static final int iv_emergency=0x7f0a0032;
-        public static final int iv_line=0x7f0a0027;
-        public static final int iv_password=0x7f0a001b;
-        public static final int iv_yizhida=0x7f0a0031;
-        public static final int left=0x7f0a0009;
-        public static final int loading_gif=0x7f0a0025;
-        public static final int login_root_layout=0x7f0a0016;
-        public static final int name_txt=0x7f0a002c;
-        public static final int none=0x7f0a0012;
-        public static final int normal=0x7f0a000e;
-        public static final int page=0x7f0a0021;
-        public static final int page_view=0x7f0a0020;
-        public static final int rg_page=0x7f0a0023;
-        public static final int right=0x7f0a000a;
-        public static final int scrollView=0x7f0a0017;
-        public static final int scrollview=0x7f0a0028;
-        public static final int spacingWidth=0x7f0a0013;
-        public static final int spacingWidthUniform=0x7f0a0014;
-        public static final int top=0x7f0a000b;
-        public static final int tv_current_tips=0x7f0a002a;
-        public static final int tv_highlight_num=0x7f0a0022;
-        public static final int tv_num=0x7f0a0030;
-        public static final int tv_opNO=0x7f0a002b;
-        public static final int update_progress=0x7f0a0033;
-        public static final int vertical=0x7f0a0010;
-        public static final int view_seperator=0x7f0a002f;
-        public static final int viewpager=0x7f0a0024;
+        public static final int LoginID=0x7f090026;
+        public static final int alwaysScroll=0x7f09000c;
+        public static final int auto_fit=0x7f090015;
+        public static final int bottom=0x7f090000;
+        public static final int btn_login=0x7f09001e;
+        public static final int center=0x7f090001;
+        public static final int center_horizontal=0x7f090002;
+        public static final int center_vertical=0x7f090003;
+        public static final int clip_horizontal=0x7f090004;
+        public static final int clip_vertical=0x7f090005;
+        public static final int columnWidth=0x7f090011;
+        public static final int disabled=0x7f09000d;
+        public static final int et_account=0x7f090019;
+        public static final int et_password=0x7f09001c;
+        public static final int fill=0x7f090006;
+        public static final int fill_horizontal=0x7f090007;
+        public static final int fill_vertical=0x7f090008;
+        public static final int gridview=0x7f09002d;
+        public static final int gvp=0x7f09001f;
+        public static final int horizontal=0x7f09000f;
+        public static final int iv_account=0x7f090018;
+        public static final int iv_avatar=0x7f090029;
+        public static final int iv_clear_account=0x7f09001a;
+        public static final int iv_clear_password=0x7f09001d;
+        public static final int iv_clip=0x7f09002e;
+        public static final int iv_emergency=0x7f090032;
+        public static final int iv_line=0x7f090027;
+        public static final int iv_password=0x7f09001b;
+        public static final int iv_yizhida=0x7f090031;
+        public static final int left=0x7f090009;
+        public static final int loading_gif=0x7f090025;
+        public static final int login_root_layout=0x7f090016;
+        public static final int name_txt=0x7f09002c;
+        public static final int none=0x7f090012;
+        public static final int normal=0x7f09000e;
+        public static final int page=0x7f090021;
+        public static final int page_view=0x7f090020;
+        public static final int rg_page=0x7f090023;
+        public static final int right=0x7f09000a;
+        public static final int scrollView=0x7f090017;
+        public static final int scrollview=0x7f090028;
+        public static final int spacingWidth=0x7f090013;
+        public static final int spacingWidthUniform=0x7f090014;
+        public static final int top=0x7f09000b;
+        public static final int tv_current_tips=0x7f09002a;
+        public static final int tv_highlight_num=0x7f090022;
+        public static final int tv_num=0x7f090030;
+        public static final int tv_opNO=0x7f09002b;
+        public static final int update_progress=0x7f090033;
+        public static final int vertical=0x7f090010;
+        public static final int view_seperator=0x7f09002f;
+        public static final int viewpager=0x7f090024;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -492,56 +483,31 @@ containing a value of this type.
         public static final int item_single_big_docotor=0x7f030007;
         public static final int progress_softupdate=0x7f030008;
     }
-    public static final class menu {
-        public static final int main=0x7f090000;
-    }
     public static final class string {
-        public static final int account=0x7f060007;
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int current_num=0x7f060003;
-        public static final int current_patient=0x7f060004;
-        public static final int hello_world=0x7f060002;
-        public static final int login=0x7f060006;
-        public static final int logining=0x7f060009;
-        public static final int next_num=0x7f060005;
-        public static final int password=0x7f060008;
-        public static final int soft_update_cancel=0x7f060010;
-        public static final int soft_update_info=0x7f06000c;
-        public static final int soft_update_later=0x7f06000e;
-        public static final int soft_update_no=0x7f06000a;
-        public static final int soft_update_title=0x7f06000b;
-        public static final int soft_update_updatebtn=0x7f06000d;
-        public static final int soft_updating=0x7f06000f;
+        public static final int account=0x7f070005;
+        public static final int app_name=0x7f070000;
+        public static final int current_num=0x7f070001;
+        public static final int current_patient=0x7f070002;
+        public static final int login=0x7f070004;
+        public static final int logining=0x7f070007;
+        public static final int next_num=0x7f070003;
+        public static final int password=0x7f070006;
+        public static final int soft_update_cancel=0x7f07000e;
+        public static final int soft_update_info=0x7f07000a;
+        public static final int soft_update_later=0x7f07000c;
+        public static final int soft_update_no=0x7f070008;
+        public static final int soft_update_title=0x7f070009;
+        public static final int soft_update_updatebtn=0x7f07000b;
+        public static final int soft_updating=0x7f07000d;
     }
     public static final class style {
         /**  activity anim 
          */
-        public static final int AnimMove=0x7f070002;
-        public static final int Anim_style=0x7f070003;
+        public static final int AnimMove=0x7f080002;
+        public static final int Anim_style=0x7f080003;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
 
     
@@ -553,29 +519,27 @@ containing a value of this type.
 
         
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int TextAppearance=0x7f070004;
-        public static final int TextAppearance_Big=0x7f070007;
-        public static final int TextAppearance_Big_Dark=0x7f070010;
-        public static final int TextAppearance_Large=0x7f070008;
-        public static final int TextAppearance_Large_Dark=0x7f070011;
-        public static final int TextAppearance_Large_Dark_Bold=0x7f070012;
-        public static final int TextAppearance_Medium=0x7f070006;
-        public static final int TextAppearance_Medium_Blue=0x7f07000f;
-        public static final int TextAppearance_Medium_Dark=0x7f07000e;
-        public static final int TextAppearance_Medium_Gray=0x7f07000d;
-        public static final int TextAppearance_Small=0x7f070005;
-        public static final int TextAppearance_Small_Dark=0x7f07000b;
-        public static final int TextAppearance_Small_Dark_Bold=0x7f07000c;
-        public static final int TextAppearance_Small_Gray=0x7f070009;
-        public static final int TextAppearance_Small_Gray_Bold=0x7f07000a;
-        public static final int loadingDialog=0x7f070013;
+        public static final int AppTheme=0x7f080001;
+        public static final int TextAppearance=0x7f080004;
+        public static final int TextAppearance_Big=0x7f080007;
+        public static final int TextAppearance_Big_Dark=0x7f080010;
+        public static final int TextAppearance_Large=0x7f080008;
+        public static final int TextAppearance_Large_Dark=0x7f080011;
+        public static final int TextAppearance_Large_Dark_Bold=0x7f080012;
+        public static final int TextAppearance_Medium=0x7f080006;
+        public static final int TextAppearance_Medium_Blue=0x7f08000f;
+        public static final int TextAppearance_Medium_Dark=0x7f08000e;
+        public static final int TextAppearance_Medium_Gray=0x7f08000d;
+        public static final int TextAppearance_Small=0x7f080005;
+        public static final int TextAppearance_Small_Dark=0x7f08000b;
+        public static final int TextAppearance_Small_Dark_Bold=0x7f08000c;
+        public static final int TextAppearance_Small_Gray=0x7f080009;
+        public static final int TextAppearance_Small_Gray_Bold=0x7f08000a;
+        public static final int loadingDialog=0x7f080013;
     }
     public static final class styleable {
         /** Attributes that can be used with a CompoundButton.
